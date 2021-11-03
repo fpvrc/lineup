d2c3008924f5a9a6fa84cd9dbd59f331c16e2876
fpvrc <jari@4midableimagery.com>
2021-11-03 16:45:32 -0700

    duapte

--- src/components/fadeInView/index.tsx
+++ src/components/fadeInView/index.tsx
@@ -1,30 +1,43 @@
 import * as React from "react";
-import { Text, View, Animated } from "react-native";
+import { Text, View } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
+import Animated, {
+  Easing,
+  withTiming,
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 const FadeInView = (props, { navigation }) => {
-  const fadeAnim = React.useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+  //const fadeAnim = React.useRef(new Animated.Value(0)).current;
+  const offset = useSharedValue(0);
 
   useFocusEffect(() => {
+    offset.value = withTiming(1, { duration: 125 });
+    /*
     Animated.timing(fadeAnim, {
       toValue: 1,
       duration: 125,
       useNativeDriver: true,
     }).start();
+    */
     return () => {
+      offset.value = withTiming(0, { duration: 62 });
+      /*
       Animated.timing(fadeAnim, {
         toValue: 0,
         duration: 62,
         useNativeDriver: true,
       }).start();
+      */
     };
   });
 
   return (
-    <Animated.View // Special animatable View
+    <Animated.View
       style={{
         flex: 1,
-        opacity: fadeAnim, // Bind opacity to animated value
+        opacity: offset.value,
       }}
     >
       {props.children}
